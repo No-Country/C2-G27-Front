9a96af882ccfe7f3e9d9d9570d91010d54199997
Fernando Alarcon <feralarcon1995@gmail.com>
2022-01-13 20:52:45 -0300

    Create: Feature component footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Fooder from "../Components/Fooder";
 import Header from "../Components/Header";
 
 export default function Home() {
 
   return <div>
     
-    
     <Header />
-    
+    <Fooder/>
+
     </div>;
 }
