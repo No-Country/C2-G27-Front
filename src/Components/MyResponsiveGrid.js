d702872210bee9a44934843d953458d3c5038971
Luis Pirela <34918215+contrastereo@users.noreply.github.com>
2022-01-17 16:51:49 -0300

    changes

---
+++ src/Components/MyResponsiveGrid.js
@@ -0,0 +1,146 @@
+import React from "react";
+import '../Assets/ReactGridLayout/styles.css';
+import '../Assets/ReactResizable/styles.css';
+import { Responsive , WidthProvider } from 'react-grid-layout';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  CardText,
+  CardImg
+} from 'reactstrap';
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+function getFromLS(key) {
+  let ls = {};
+  if (global.localStorage) {
+    try {
+      ls = JSON.parse(global.localStorage.getItem("rgl-8")) || {};
+    } catch (e) {
+      /* Ignore */
+    }
+  }
+  return ls[key];
+}
+const originalLayouts = getFromLS("layouts") || {};
+
+function saveToLS(key, value) {
+  if (global.localStorage) {
+    global.localStorage.setItem(
+      "rgl-8",
+      JSON.stringify({
+        [key]: value
+      })
+      );
+    }
+  }
+
+    export default class ResponsiveLocalStorageLayout extends React.PureComponent {
+      static defaultProps() {        
+      return {
+        className: "layout",
+        cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },      
+        rowHeight: 30
+      };
+      }
+
+      constructor(props) {
+        super(props);        
+        this.state = {
+          layouts: JSON.parse(JSON.stringify(originalLayouts))
+        };
+      }
+
+
+
+      onLayoutChange(layout, layouts) {
+      saveToLS("layouts", layouts);
+      this.setState({ layouts });
+    }
+
+    resetLayout() {
+      this.setState({ layouts: {} });
+    }   
+
+    render() {
+      const {layouts}=this.state;      
+      return (
+        <div>
+            <Button onClick={() => this.resetLayout()}>Reset Layout</Button>
+            <ResponsiveReactGridLayout
+              className="layout"
+              cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+              rowHeight={30}
+              layouts={layouts}
+              onLayoutChange={(layout, layoutss) =>{
+                this.onLayoutChange(layout, layoutss);
+                console.log(layouts);
+              }
+              }
+              >
+              <Card key="4" data-grid={{ w: 2, h: 8, x: 0, y: 0, minW: 2, minH:8 }} className='bg-info g-md-0'>
+                <CardHeader>
+                  <CardTitle>Widget #1</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <CardImg
+                    alt="Card image cap"
+                    src="https://picsum.photos/318/180"
+                    top
+                    width="100%"
+                    className="img-fluid"
+                  />
+                  <CardText>
+                    Texto descripción de la tarjeta.
+                  </CardText>
+                  <Button variant='secondary' type='submit'>
+                    Explore
+                  </Button>
+                </CardBody>
+              </Card>
+              <Card key="5" data-grid={{ isDraggable: false, w: 2, h: 6, x: 2, y: 0, minW: 2, minH: 6}} className='bg-info'>
+                <CardHeader>
+                  <CardTitle>Widget #2</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <CardText>
+                    <p>
+                      Texto descripción de la tarjeta.
+                    </p>
+                    <p>
+                      Más texto.
+                    </p>
+                    <p>
+                      y más.
+                    </p>
+                  </CardText>
+                  <Button variant='secondary' type='submit'>
+                    Explore
+                  </Button>
+                </CardBody>
+              </Card>
+              <Card key="6" data-grid={{ w: 2, h: 4, x: 4, y: 1, minW: 2, minH: 4 }} className='bg-info'>
+            <CardHeader>
+              <CardTitle>Widget #3</CardTitle>
+            </CardHeader>
+            <CardBody>
+              <CardText>
+                Texto descripción de la tarjeta.
+              </CardText>
+              <Button variant='secondary' type='submit'>
+                Explore
+              </Button>
+            </CardBody>
+          </Card>           
+            </ResponsiveReactGridLayout>
+          </div>
+        );
+      }
+    }
+
+
+
+    /* export default MyResponsiveGrid */
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,40 +1,37 @@
 import React from 'react';
-import { Container, Row, Col } from "reactstrap"
-import { FaHome } from 'react-icons/fa'
+import { Container, Row, Col } from 'reactstrap';
+import { FaHome } from 'react-icons/fa';
 import Fooder from '../Components/Fooder';
 import SearchBar from '../Components/SearchBar';
 import UserCard from '../Components/UserCard';
 import WidgedBoard from '../Components/WidgedBoard';
 
-
 export default function Home() {
   return (
-
-
     <Container className='d-flex flex-column  flex-fill mt-2'>
       <Row className='mb-2'>
-        <Col >
-          <h1 className='homeTitle'><FaHome /> / Dashboard </h1>
+        <Col>
+          <h1 className='homeTitle'>
+            <FaHome /> / Dashboard{' '}
+          </h1>
         </Col>
-        <Col className="d-flex" >
+        <Col className='d-flex'>
           <SearchBar />
         </Col>
         <Col>
           <UserCard />
         </Col>
       </Row>
-      <Row className='' >
+      <Row className=''>
         <Col>
           <WidgedBoard />
         </Col>
       </Row>
-      <Row className='' >
+      <Row className=''>
         <Col>
           <Fooder />
         </Col>
       </Row>
     </Container>
-
-
   );
 }
