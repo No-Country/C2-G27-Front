f45abe9d6d3c7aeb9d79302d5feb430ba5c31b9b
Luis Pirela <34918215+contrastereo@users.noreply.github.com>
2022-01-20 22:55:01 -0300

    navigation enabled

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, NavLink, NavItem} from 'reactstrap';
+import { Nav, NavLink, NavItem } from 'reactstrap';
 import HomeIcon from '@mui/icons-material/Home';
 import AssessmentIcon from '@mui/icons-material/Assessment';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
@@ -8,61 +8,50 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SettingsIcon from '@mui/icons-material/Settings';
 
-
-
 export default function Navigation() {
-
-
-  return (<div>
-
-    
-
-    <div className='sidebar'>
-      <p>
-        CriptoTech
-      </p>
-      <hr />
-      <Nav vertical>
-        <NavItem>
-          <NavLink href="#">
-            <HomeIcon />  Home
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">
-            <FactCheckIcon /> Summary
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">
-            <MonetizationOnIcon /> Assets
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#" >
-            <AssessmentIcon /> Metric
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#" >
-            <AccountCircleIcon /> Profile
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#" >
-            <SettingsIcon /> Settings
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#" >
-            <LogoutIcon /> Log Out
-          </NavLink>
-        </NavItem>
-      </Nav>
-
+  return (
+    <div>
+      <div className='sidebar'>
+        <p>CriptoTech</p>
+        <hr />
+        <Nav vertical>
+          <NavItem>
+            <NavLink href='/'>
+              <HomeIcon /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/admin'>
+              <FactCheckIcon /> Admin Panel
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='#'>
+              <MonetizationOnIcon /> Assets
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='#'>
+              <AssessmentIcon /> Metric
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='#'>
+              <AccountCircleIcon /> Profile
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='#'>
+              <SettingsIcon /> Settings
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='#'>
+              <LogoutIcon /> Log Out
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
     </div>
-
-  </div>
-
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
+import { useLocation } from 'react-router-dom';
 import Footer from '../Components/Fooder';
 import WidgetsBoard from '../Components/WidgetsBoard';
 import SearchBar from '../Components/SearchBar';
@@ -7,14 +9,16 @@ import UserCard from '../Components/UserCard';
 import NavBarResponsive from '../Components/NavBarResponsive';
 
 export default function Home() {
+  const location = useLocation();
+
+  console.log(location);
   return (
     <div>
-      <NavBarResponsive/>
+      <NavBarResponsive />
       <Container>
-        <Row >
-
+        <Row>
           <Col className='userPanel '>
-            <Row className="d-flex fluid justify-content-between mt-2 mb-2 ">
+            <Row className='d-flex fluid justify-content-between mt-2 mb-2 '>
               <Col>
                 <SearchBar />
               </Col>
