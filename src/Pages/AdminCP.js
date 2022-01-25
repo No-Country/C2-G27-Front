1d3d46d2f25a7e7a98882a02aca14676d4f41cbf
Fer Alarcon <fertkd95@gmail.com>
2022-01-25 13:25:48 -0300

    add styles to admin panel and footer of dashboard

--- src/Pages/AdminCP.js
+++ src/Pages/AdminCP.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Container, Button, Row, Col } from 'reactstrap';
+import NavBarResponsive from '../Components/NavBarResponsive';
+import Footer from '../Components/FooterDash';
+
 
 export default function AdminCP() {
   return (
-    <div>
-      <Container className='d-flex align-items-center justify-content-center vw-100 vh-100 '>
+    <div  style={{width:'100%'}}>
+      <NavBarResponsive />
+      <div className='banner-panel'>
+         <h2>Welcome to Admin Panel</h2>
+      </div>
+      <Container className='d-flex align-items-center justify-content-center  vh-100 '>
         <Row >
           <Col className='d-flex flex-column'>
-            <Button className="mb-2">Users Assets</Button>
-            <Button className="mb-2">Company Assets</Button>
-            <Button className="mb-2">Manage Users</Button>
-            <Button className="mb-2">Search User</Button>
-            <Button>Create User</Button>
+            <Button className="mb-2" color="primary">Users Assets</Button>
+            <Button className="mb-2" color="primary">Company Assets</Button>
+            <Button className="mb-2" color="primary">Manage Users</Button>
+            <Button className="mb-2" color="primary">Search User</Button>
+            <Button color="primary">Create User</Button>
           </Col>
         </Row>
       </Container>
+      <Footer />
+
     </div>
   );
 }
